e51417a7d7ab5fd596ccb8e710102f71a801734e
Abhipawar448 <abhijeetpawar448@gmail.com>
2024-06-28 12:48:45 +0530

    adding abhi test file

---
+++ array2.java
@@ -0,0 +1,16 @@
+class array1
+{
+    public static void main(String []args)
+    {
+
+        int[] a ={1,2,3,4,5,6,7};
+       
+       for(int i=0;i<a.length;++i)
+       {
+           System.out.println("this are the element in the array   " +a[1]);
+            System.out.println("this are the element in the array   " +a[4]);
+          
+
+       }
+    }
+}
---
+++ fib.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class fib
+{
+    public static void main(String []args)
+    {
+           int n1=0,n2=1,t,num;
+        Scanner a =new Scanner(System.in);
+        num=a.nextInt();
+        System.out.println("enter a num");
+        
+
+     
+
+        while (true)
+        {
+            t=n1+n2;
+            if(t==num)
+            {
+                System.out.println("the no is fibonacchi ");
+                break;
+
+            }
+            else if(t>num)
+            {
+                System.out.println("it is not a fib no");
+                break;
+
+            }
+            n1=n2;
+            n2=t;
+
+        }
+
+
+    }
+}
